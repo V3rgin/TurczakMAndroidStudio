20e55ae1efb5d61cc37ba5f7db81ac10e466e37b
Julian <julianszkudlarek12@gmail.com>
2024-09-12 13:21:12 +0200

    Disable button update, reset button, counter

--- app/src/main/java/com/example/turczakMAndriodstudio/MainActivity.java
+++ app/src/main/java/com/example/turczakMAndriodstudio/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button bdisable;
-
+    private Button breset;
+    private TextView counter;
+    int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +24,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 bdisable.setEnabled(false);
+                count++;
+                counter.setText("counter: " + count);
             }
         });
+        breset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bdisable.setEnabled(true);
+            }
+        });
+
     }
 }
